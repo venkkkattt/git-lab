7e1fc8f562e93fa14789adc70a64600d11c5db00
venkkkattt <lsvenkateshwaran1064@gmail.com>
2025-04-02 10:06:00 +0530

    index

---
+++ src/test/java/webpg.java
@@ -0,0 +1,34 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertTrue;
+
+public class webpg {
+    private static WebDriver driver;
+
+    @BeforeTest
+    public void openBrowser() throws InterruptedException {
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        Thread.sleep(2000);
+        driver.get("https://github.com/venkkkattt/git-lab.git");
+    }
+
+    @Test
+    public void titleValidationTest(){
+        String actualTitle = driver.getTitle();
+        String expectedTitle = "Sample Website";
+        Assert.assertEquals(actualTitle, expectedTitle);
+
+    }
+
+    @AfterTest
+    public void closeBrowser() throws InterruptedException {
+        Thread.sleep(1000);
+        driver.quit();
+    }
+}
