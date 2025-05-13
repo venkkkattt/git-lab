5db89ba0febf038d422cbf3378bf73db65a262f8
venkkkattt <lsvenkateshwaran1064@gmail.com>
2025-05-13 20:22:30 +0530

    gradlep2

---
+++ build.gradle.kts
@@ -0,0 +1,42 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+plugins {
+    id("java")
+
+    kotlin("jvm") version "1.8.10"
+    application
+}
+
+group = "com.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(kotlin("stdlib"))
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions.jvmTarget = "17"
+}
+application {
+    mainClass.set("com.example.Main")
+}
+
+tasks.register<Jar>("fatJar") {
+    archiveClassifier.set("all")
+    duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+            manifest {
+        attributes["Main-Class"] = "com.example.Main"
+        }
+    from(configurations.runtimeClasspath.get().map { if (it.isDirectory) it else zipTree(it) })
+
+    with(tasks.jar.get() as CopySpec)
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,1 @@
+rootProject.name = "gradlep2"
