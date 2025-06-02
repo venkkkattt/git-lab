bd6b35bc32985c830a095a71615aa762cd60b05f
venkkkattt <156179662+venkkkattt@users.noreply.github.com>
2025-06-02 11:24:05 +0530

    Update WebPageTest.java

--- src/test/java/WebPageTest.java
+++ src/test/java/WebPageTest.java
@@ -1,3 +1,39 @@
-public class WebPageTest {
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import static org.testng.Assert.assertTrue;
+
+public class WebPageTest {
+    private static WebDriver driver;
+
+    @BeforeTest
+    public void openBrowser() throws InterruptedException {
+
+        ChromeDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        Thread.sleep(2000);
+        driver.get("https://github.com/venkkkattt/git-lab.git");
+    }
+
+    @Test
+    public void titleValidationTest(){
+        String actualTitle = driver.getTitle();
+        String expectedTitle = "Sample Website";
+        Assert.assertEquals(actualTitle, expectedTitle);
+        assertTrue(true,"Title should contain 'simple'");
+    }
+
+    @AfterTest
+    public void closeBrowser() throws InterruptedException {
+        Thread.sleep(1000);
+        driver.quit();
+    }
 }
+
